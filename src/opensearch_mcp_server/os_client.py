0dad423c59b7d14aaf502222eea367e0bd03bd12
jimmy974 <jimmy974@gmail.com>
2025-03-17 16:10:57 +0000

    initial add

---
+++ src/opensearch_mcp_server/os_client.py
@@ -0,0 +1,40 @@
+import logging
+import os
+from dotenv import load_dotenv
+from opensearchpy import OpenSearch
+import warnings
+
+class OpensearchClient:
+    def __init__(self, logger: logging.Logger):
+        self.logger = logger
+        self.os_client = self._create_opensearch_client()
+
+    def _get_os_config(self):
+        """Get Opensearch configuration from environment variables."""
+        # Load environment variables from .env file
+        load_dotenv()
+        config = {
+            "host": os.getenv("OPENSEARCH_HOST"),
+            "username": os.getenv("OPENSEARCH_USERNAME"),
+            "password": os.getenv("OPENSEARCH_PASSWORD")
+        }
+        
+        if not all([config["username"], config["password"]]):
+            self.logger.error("Missing required Opensearch configuration. Please check environment variables:")
+            self.logger.error("OPENSEARCH_USERNAME and OPENSEARCH_PASSWORD are required")
+            raise ValueError("Missing required Opensearch configuration")
+        
+        return config
+
+    def _create_opensearch_client(self) -> OpenSearch:
+        """Create and return an Opensearch client using configuration from environment."""
+        config = self._get_os_config()
+
+        # Disable SSL warnings
+        warnings.filterwarnings("ignore", message=".*TLS with verify_certs=False is insecure.*",)
+
+        return OpenSearch(
+            config["host"],
+            http_auth=(config["username"], config["password"]),
+            verify_certs=False
+        )
